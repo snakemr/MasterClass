0f11c7e865419473c253006fa99a2080e3c4ee46
mad.lrmk.ru <admin@lrmk.ru>
2023-03-14 13:04:00 +0300

    full

--- app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
+++ app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
@@ -2,14 +2,16 @@ package ru.lrmk.masterclass
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Surface
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -24,16 +26,27 @@ class MainActivity : ComponentActivity() {
                     val movies by produceState(emptyList<Movie>()) {
                         value = api.movies()
                     }
-                    LazyVerticalGrid(GridCells.Adaptive(120.dp)) {
+                    var movie by remember {
+                        mutableStateOf(0)
+                    }
+                    BackHandler(movie != 0) {
+                        movie = 0
+                    }
+                    if (movie == 0) LazyVerticalGrid(GridCells.Adaptive(120.dp)) {
                         items(movies) {
                             AsyncImage(model = API.small + it.poster_path,
                                 contentDescription = "",
                                 Modifier.size(200.dp)
+                                    .clickable { movie = it.id }
                             )
                         }
                     }
+                    else Movie(movies.first { it.id == movie })
                 }
             }
         }
     }
 }
+
+@Composable
+fun Movie(movie: Movie) = Text(movie.name)
