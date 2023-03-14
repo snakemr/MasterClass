3918ec6bee140e90525a5a643efd6e7f396a9e2e
mad.lrmk.ru <admin@lrmk.ru>
2023-03-14 11:54:08 +0300

    half hour

--- app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
+++ app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
@@ -2,32 +2,38 @@ package ru.lrmk.masterclass
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import ru.lrmk.masterclass.ui.theme.MasterClassTheme
-import ru.lrmk.masterclass.ui.theme.Player
-import ru.lrmk.masterclass.ui.theme.shadowText
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MasterClassTheme(darkTheme = true) {
+                Surface {
+                    val movies by produceState(emptyList<Movie>()) {
+                        value = api.movies()
+                    }
+                    LazyVerticalGrid(GridCells.Adaptive(120.dp)) {
+                        items(movies) {
+                            AsyncImage(model = API.small + it.poster_path,
+                                contentDescription = "",
+                                Modifier.size(200.dp)
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
