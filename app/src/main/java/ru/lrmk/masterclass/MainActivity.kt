280738c172d1cfdc72837affb62c2391160c81b6
mad.lrmk.ru <admin@lrmk.ru>
2023-03-14 12:09:16 +0300

    clear

--- app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
+++ app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
@@ -28,6 +28,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MasterClassTheme(darkTheme = true) {
+                Surface {
+
+                }
+            }
         }
     }
 }
