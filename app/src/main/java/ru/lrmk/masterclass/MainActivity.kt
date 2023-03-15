0302933260745f6af958f0f84e7668871b3c5240
mad.lrmk.ru <admin@lrmk.ru>
2023-03-15 12:15:37 +0300

    full

--- app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
+++ app/src/main/java/ru/lrmk/masterclass/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -16,9 +14,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import ru.lrmk.masterclass.ui.theme.MasterClassTheme
+import ru.lrmk.masterclass.ui.theme.Player
+import ru.lrmk.masterclass.ui.theme.shadowText
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,7 +57,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Movie(movie: Movie) = Box {
-    AsyncImage(model = API.big + movie.backdrop_path, contentDescription = "",
-        Modifier.fillMaxSize().alpha(0.6f)
+    var show by remember {
+        mutableStateOf(false)
+    }
+    BackHandler(show) {
+        show = false
+    }
+    AsyncImage(model = API.big + movie.backdrop_path,
+        contentDescription = "", Modifier.fillMaxSize().alpha(0.6f),
+        contentScale = ContentScale.FillHeight
     )
+    if (show)
+        Player(videoURI = API.video + movie.video)
+    else Column {
+        Row(Modifier.fillMaxWidth().padding(10.dp), Arrangement.SpaceBetween) {
+            Text(movie.vote_average.toString(), fontSize = 20.sp, style = shadowText)
+            Text(movie.release_date.toString().takeLast(4), fontSize = 20.sp, style = shadowText)
+        }
+        Text(movie.name, Modifier.fillMaxWidth(),
+            fontSize = 24.sp, style = shadowText, textAlign = TextAlign.Center
+        )
+        Text(movie.overview, Modifier.padding(10.dp),
+            style = shadowText, textAlign = TextAlign.Justify
+        )
+        AsyncImage(model = API.big + movie.poster_path, contentDescription = "",
+            Modifier.fillMaxWidth().clickable { show = true }.padding(10.dp)
+        )
+    }
 }
